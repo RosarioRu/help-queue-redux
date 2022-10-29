8cce8c4b608521203251ab6f036a538a14cb7eeb
RosarioRu <mariarruval@gmail.com>
2022-10-29 16:46:24 -0700

    Write second test to add a new ticket to TicketList and ensure it passes.

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,6 +1,19 @@
 
 const reducer = (state = {}, action) => {
-
+  const { type, names, location, issue, id } = action;
+  switch(type) {
+  case "ADD_TICKET":
+    return Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+  default: 
+    return state;
+  }
 };
 
 export default reducer;
